a88f89c70c8360e9fbedda88351650a95fdcc8bb
Mayur Odedra <mayurodedra.40ST@gmail.com>
2025-07-17 12:55:34 +0200

    converted to pwa

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import './css/landingpages.css';
-const button = document.getElementById('start-button');
+import "./css/landingpages.css";
+const button = document.getElementById("start-button");
 let continueIDOK = false;
 
 const subjID =
-  new URL(document.location.href).searchParams.get('ID') || 'testID';
+  new URL(document.location.href).searchParams.get("ID") || "testID";
 
 // FOR INPUT FORM
 
-
 // FOR WEBCAM RECORIDING
 // get both radio buttons
-const webcamOptions = document.getElementsByName('webcam-options');
-let webcam = 'false'; // no as default
+const webcamOptions = document.getElementsByName("webcam-options");
+let webcam = "false"; // no as default
 
 for (const option of webcamOptions) {
   option.onclick = () => {
@@ -25,7 +24,13 @@ for (const option of webcamOptions) {
 // define what happens on button click
 const handleContinueClick = (event) => {
   event.preventDefault();
-  window.location.href = `./instructions.html?ID=${subjID}&webcam=${webcam}`;
+  const studyChoices = {
+    ID: subjID,
+    webcam: webcam,
+  };
+
+  localStorage.setItem("storedChoices", JSON.stringify(studyChoices));
+  window.location.href = `./instructions.html`;
 };
 
-button.addEventListener('click', handleContinueClick);
+button.addEventListener("click", handleContinueClick);
--- src/prabat.js
+++ src/prabat.js
@@ -1,22 +1,32 @@
-import './css/prabat.css';
-import * as mrec from '@ccp-eva/media-recorder';
-import * as DetectRTC from 'detectrtc';
+import "./css/prabat.css";
+import * as mrec from "@ccp-eva/media-recorder";
+import * as DetectRTC from "detectrtc";
 
-import { downloadData } from './js/downloadData.js';
-import { uploadData } from './js/uploadData.js';
-import { pause } from './js/pause.js';
+import { downloadData } from "./js/downloadData.js";
+import { uploadData } from "./js/uploadData.js";
+import { uploadVideo } from "./js/uploadVideo.js";
+import { downloadVideo } from "./js/downloadVideo.js";
+import { pause } from "./js/pause.js";
 // import { hideURLparams } from './js/hideURLparams.js';
-import { openFullscreen } from './js/openFullscreen.js';
-import { checkForTouchscreen } from './js/checkForTouchscreen.js';
+import { openFullscreen } from "./js/openFullscreen.js";
+import { checkForTouchscreen } from "./js/checkForTouchscreen.js";
 // import { randomizeNewTrials } from './js/randomizeNewTrials.js';
 
-document.addEventListener('DOMContentLoaded', function () {
+const storedChoices = localStorage.getItem("storedChoices");
+let studyChoices;
+if (storedChoices) {
+  studyChoices = JSON.parse(storedChoices);
+} else {
+  console.error("No data found in local storage");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   const devmode = false;
 
   //------------------------------------------------------------------
   // automatically add running trial numbers as ids to html
   //------------------------------------------------------------------
-  const trialDivs = document.querySelectorAll('.trials');
+  const trialDivs = document.querySelectorAll(".trials");
 
   // Iterate over trial divs and set their IDs
   trialDivs.forEach((div, index) => {
@@ -29,13 +39,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const responseLog = {
     // get ID out of URL parameter
     meta: {
-      subjID:
-        new URL(document.location.href).searchParams.get('ID') || 'testID',
-      order: window.location.pathname.split('/').pop().replace('.html', ''),
+      subjID: studyChoices.ID || "testID",
+      order: window.location.pathname.split("/").pop().replace(".html", ""),
       touchscreen: checkForTouchscreen(),
-      webcam:
-        new URL(document.location.href).searchParams.get('webcam') === 'true' ||
-        false,
+      webcam: studyChoices.webcam || false,
     },
     data: [],
   };
@@ -67,13 +74,15 @@ document.addEventListener('DOMContentLoaded', function () {
   //------------------------------------------------------------------
   // get relevant elements
   //------------------------------------------------------------------
-  const allAudios = document.getElementsByTagName('audio');
-  const betweenTrials = document.getElementById('between-trials');
-  const betweenTrialsBackground = document.getElementById('between-trials-background');
-  const button = document.getElementById('prabat-button');
-  const speaker = document.getElementById('speaker');
-  const headingFullscreen = document.getElementById('heading-fullscreen');
-  const headingTestsound = document.getElementById('heading-testsound');
+  const allAudios = document.getElementsByTagName("audio");
+  const betweenTrials = document.getElementById("between-trials");
+  const betweenTrialsBackground = document.getElementById(
+    "between-trials-background"
+  );
+  const button = document.getElementById("prabat-button");
+  const speaker = document.getElementById("speaker");
+  const headingFullscreen = document.getElementById("heading-fullscreen");
+  const headingTestsound = document.getElementById("heading-testsound");
   //------------------------------------------------------------------
   // define response click
   //------------------------------------------------------------------
@@ -81,21 +90,21 @@ document.addEventListener('DOMContentLoaded', function () {
     event.preventDefault();
 
     // Prevent clicks on the img element with id="character"
-    if (event.target.id === 'character' || event.target.id === 'transition') {
+    if (event.target.id === "character" || event.target.id === "transition") {
       return;
     }
 
     t1 = new Date().getTime();
 
     const currentTrial = document.getElementById(`trial${trialNr - 1}`);
-    const currentImages = Array.from(currentTrial.getElementsByTagName('img'));
+    const currentImages = Array.from(currentTrial.getElementsByTagName("img"));
     currentImages.forEach((img) => {
-      if (img.id !== 'character' && img.id !== 'background') {
-      img.style.border = 'transparent';
+      if (img.id !== "character" && img.id !== "background") {
+        img.style.border = "transparent";
       }
     });
 
-    event.target.style.border = '0.3vw solid blue';
+    event.target.style.border = "0.3vw solid blue";
 
     // save response
     // trial - 2 since array starts at zero (-1) and continue click already advanced trial count (-1)
@@ -106,28 +115,28 @@ document.addEventListener('DOMContentLoaded', function () {
       // split('/').pop(): splits string at / and keeps only last element
       // then remove N_ and .jpg
       targetWord: allAudios[trialNr - 1].src
-        .split('/')
+        .split("/")
         .pop()
-        .replace('N_', '')
-        .replace('V_', '')
-        .replace('zm_', '')
-        .replace('.mp3', ''),
+        .replace("N_", "")
+        .replace("V_", "")
+        .replace("zm_", "")
+        .replace(".mp3", ""),
       chosenWord: event.target.src
-        .split('/')
+        .split("/")
         .pop()
-        .replace('N_', '')
-        .replace('V_', '')
-        .replace('.jpeg', ''),
+        .replace("N_", "")
+        .replace("V_", "")
+        .replace(".jpeg", ""),
       chosenCategory: event.target.dataset.wordCategory,
       chosenPosition: event.target.classList[0],
-      wordClass: allAudios[trialNr - 1].src.split('/').pop().startsWith('N_')
-        ? 'noun'
-        : allAudios[trialNr - 1].src.split('/').pop().startsWith('V_')
-        ? 'verb'
-        : 'unknown',
+      wordClass: allAudios[trialNr - 1].src.split("/").pop().startsWith("N_")
+        ? "noun"
+        : allAudios[trialNr - 1].src.split("/").pop().startsWith("V_")
+        ? "verb"
+        : "unknown",
     };
 
-    button.addEventListener('click', handleContinueClick, {
+    button.addEventListener("click", handleContinueClick, {
       capture: false,
       once: true,
     });
@@ -138,26 +147,27 @@ document.addEventListener('DOMContentLoaded', function () {
   //------------------------------------------------------------------
   const handleContinueClick = async (event) => {
     event.preventDefault();
-    
+
     if (devmode) {
-      console.log('trialNr', trialNr);
+      console.log("trialNr", trialNr);
       console.log(allAudios[trialNr]);
       console.log(responseLog);
     }
 
     // enable fullscreen and have short break, before first trial starts
     if (trialNr === 0) {
-      if (!devmode & !responseLog.meta.iOSSafari); openFullscreen();
-      headingFullscreen.style.display = 'none';
-      headingTestsound.style.display = 'inline';
-      speaker.setAttribute('visibility', 'visible');
+      if (!devmode & !responseLog.meta.iOSSafari);
+      openFullscreen();
+      headingFullscreen.style.display = "none";
+      headingTestsound.style.display = "inline";
+      speaker.setAttribute("visibility", "visible");
       await pause(1000);
       // for safari, first sound needs to happen on user interaction
       allAudios[trialNr].play();
 
       await pause(1000);
 
-      button.addEventListener('click', handleContinueClick, {
+      button.addEventListener("click", handleContinueClick, {
         capture: false,
         once: true,
       });
@@ -165,73 +175,76 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // end of trials
     if (trialNr === trialDivs.length) {
-      downloadData(responseLog.data, responseLog.meta.subjID);
-      uploadData(responseLog.data, responseLog.meta.subjID);
-
-      // save the video locally
-      if (!responseLog.meta.iOSSafari && responseLog.meta.webcam) {
-        mrec.stopRecorder();
-
-        // give some time to create Video Blob
-
-        const day = new Date().toISOString().substring(0, 10);
-        const time = new Date().toISOString().substring(11, 19);
-
-        setTimeout(
-          () =>
-            mrec.downloadVideo(
-              `prabat-${responseLog.meta.subjID}-${day}-${time}`,
-            ),
-          1000,
-        );
-      }
-
-      await pause(1000);
-
-      window.location.href = `./goodbye.html?ID=${responseLog.meta.subjID}`;
+      await uploadVideo(
+        responseLog.meta.iOSSafari,
+        responseLog.meta.webcam,
+        responseLog.meta.subjID,
+        mrec
+      );
+      await pause(3000);
+      await downloadVideo(
+        responseLog.meta.iOSSafari,
+        responseLog.meta.webcam,
+        responseLog.meta.subjID,
+        mrec
+      );
+      await pause(2000);
+      await uploadData(responseLog.data, responseLog.meta.subjID);
+      await pause(2000);
+      await downloadData(responseLog.data, responseLog.meta.subjID);
+      await pause(2000);
+      studyChoices.ID = responseLog.meta.subjID;
+      window.location.href = `./goodbye.html`;
+      
     }
 
     // Story
 
     if (trialNr === 1) {
       const images = [
-        'images/backgrounds/start_1_waving.svg',
-        'images/backgrounds/start_1.svg',
-        'images/backgrounds/start_2.svg',
-        'images/backgrounds/start_3.svg',
-        'images/backgrounds/start_4.svg'
+        "images/backgrounds/start_1_waving.svg",
+        "images/backgrounds/start_1.svg",
+        "images/backgrounds/start_2.svg",
+        "images/backgrounds/start_3.svg",
+        "images/backgrounds/start_4.svg",
       ];
-    
+
       let currentIndex = 0;
-      const imgElement = document.getElementById('background');
-      const ContinueStar = document.getElementById('continue-star');
-    
+      const imgElement = document.getElementById("background");
+      const ContinueStar = document.getElementById("continue-star");
+
       function showNextImage() {
         if (currentIndex <= images.length) {
           imgElement.src = images[currentIndex];
-          if (images[currentIndex] === 'images/backgrounds/start_1_waving.svg') {
-          setTimeout(showNextImage, 3000);
-          }
-          else if (images[currentIndex] === 'images/backgrounds/start_1.svg') {
+          if (
+            images[currentIndex] === "images/backgrounds/start_1_waving.svg"
+          ) {
+            setTimeout(showNextImage, 3000);
+          } else if (
+            images[currentIndex] === "images/backgrounds/start_1.svg"
+          ) {
             setTimeout(showNextImage, 2500);
-          }
-          else if (images[currentIndex] === 'images/backgrounds/start_2.svg') {
+          } else if (
+            images[currentIndex] === "images/backgrounds/start_2.svg"
+          ) {
             setTimeout(showNextImage, 6000);
-          }
-          else if (images[currentIndex] === 'images/backgrounds/start_3.svg') {
+          } else if (
+            images[currentIndex] === "images/backgrounds/start_3.svg"
+          ) {
             setTimeout(showNextImage, 9000);
-          }
-          else if (images[currentIndex] === 'images/backgrounds/start_4.svg') {
-            ContinueStar.style.opacity = '1';
+          } else if (
+            images[currentIndex] === "images/backgrounds/start_4.svg"
+          ) {
+            ContinueStar.style.opacity = "1";
           }
 
           currentIndex++;
         }
       }
-    
+
       showNextImage(); // Start the slideshow
 
-      button.addEventListener('click', handleContinueClick, {
+      button.addEventListener("click", handleContinueClick, {
         capture: false,
         once: true,
       });
@@ -241,23 +254,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // transition images for trials that need multiple slides
 
-    if (currentTrial.classList.contains('transitionSlide')) {
+    if (currentTrial.classList.contains("transitionSlide")) {
       betweenTrialsBackground.src = "images/backgrounds/background_empty.svg";
-      headingTestsound.style.display = 'none';
+      headingTestsound.style.display = "none";
       // pause audio (that might be playing if speaker item was clicked and prompt was repeated)
       allAudios[trialNr - 1].pause();
       allAudios[trialNr - 1].currentTime = 0;
       const lastTrial = document.getElementById(`trial${trialNr - 1}`);
-      lastTrial.style.display = 'none';
+      lastTrial.style.display = "none";
 
-      betweenTrials.style.display = 'flex';
+      betweenTrials.style.display = "flex";
       betweenTrialsBackground.style.opacity = 1;
       //await pause(150);
 
-      const trialAudio = currentTrial.querySelector('audio');
+      const trialAudio = currentTrial.querySelector("audio");
 
-      if (currentTrial.classList.contains('silent') == false) {
-       // play audio of current trial
+      if (currentTrial.classList.contains("silent") == false) {
+        // play audio of current trial
         if (trialAudio) {
           trialAudio.play();
         }
@@ -268,76 +281,80 @@ document.addEventListener('DOMContentLoaded', function () {
       // save response time start point
       //t0 = new Date().getTime();
 
-      betweenTrials.style.display = 'none';
+      betweenTrials.style.display = "none";
 
       //document.body.style.backgroundImage = "url('images/backgrounds/background01.png')";
       document.body.style.backgroundSize = "cover";
       document.body.style.backgroundPosition = "center";
-      const flexWrapper = document.getElementById('flex-wrapper');
-      flexWrapper.style.backgroundColor = 'transparent';
+      const flexWrapper = document.getElementById("flex-wrapper");
+      flexWrapper.style.backgroundColor = "transparent";
       //betweenTrials.style.display = 'none';
 
-      
-      currentTrial.style.display = 'block';
+      currentTrial.style.display = "block";
 
-      if (currentTrial.classList.contains('silent')) {
+      if (currentTrial.classList.contains("silent")) {
         // Wait for the animation to end, then continue
-        currentTrial.addEventListener('animationend', async () => {
-          await pause(2000);
-          handleContinueClick(new Event('click'));
-        }, { once: true });
+        currentTrial.addEventListener(
+          "animationend",
+          async () => {
+            await pause(2000);
+            handleContinueClick(new Event("click"));
+          },
+          { once: true }
+        );
       } else {
-          trialAudio.onended = async () => {
+        trialAudio.onended = async () => {
           await pause(2000);
-          handleContinueClick(new Event('click'));
+          handleContinueClick(new Event("click"));
         };
       }
     }
 
     // hide last Trial, show background (empty pictures) instead
-    if (trialNr > 0 && currentTrial.classList.contains('transitionSlide') == false) {
+    if (
+      trialNr > 0 &&
+      currentTrial.classList.contains("transitionSlide") == false
+    ) {
       const currentTrial = document.getElementById(`trial${trialNr}`);
-      headingTestsound.style.display = 'none';
+      headingTestsound.style.display = "none";
       // pause audio (that might be playing if speaker item was clicked and prompt was repeated)
       allAudios[trialNr - 1].pause();
       allAudios[trialNr - 1].currentTime = 0;
       const lastTrial = document.getElementById(`trial${trialNr - 1}`);
-      lastTrial.style.display = 'none';
+      lastTrial.style.display = "none";
 
-      betweenTrials.style.display = 'flex';
+      betweenTrials.style.display = "flex";
       betweenTrialsBackground.style.opacity = 1;
       //await pause(150);
 
       // play audio of current trial
       // Play the audio element contained in currentTrial
-      const trialAudio = currentTrial.querySelector('audio');
+      const trialAudio = currentTrial.querySelector("audio");
       if (trialAudio) {
         trialAudio.play();
       }
 
-
       // save response time start point
       t0 = new Date().getTime();
 
-      betweenTrials.style.display = 'none';
-
+      betweenTrials.style.display = "none";
 
       //document.body.style.backgroundImage = "url('images/backgrounds/background01.png')";
       //document.body.style.backgroundSize = "cover";
       //document.body.style.backgroundPosition = "center";
-      const flexWrapper = document.getElementById('flex-wrapper');
-      flexWrapper.style.backgroundColor = 'transparent';
+      const flexWrapper = document.getElementById("flex-wrapper");
+      flexWrapper.style.backgroundColor = "transparent";
       //betweenTrials.style.display = 'none';
 
-      currentTrial.style.display = 'block';
+      currentTrial.style.display = "block";
 
       const currentImages = Array.from(
-        currentTrial.getElementsByTagName('img'),
+        currentTrial.getElementsByTagName("img")
       );
 
       trialAudio.onended = () => {
         currentImages.forEach((img) => {
-          img.addEventListener('click', handleResponseClick, {
+          img.addEventListener("click", handleResponseClick, {
             capture: false,
             once: false,
           });
@@ -362,18 +379,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // play audio of current trial
     allAudios[trialNr - 1].play();
-
   };
 
   //------------------------------------------------------------------
   // add eventListeners
   //------------------------------------------------------------------
-  button.addEventListener('click', handleContinueClick, {
+  button.addEventListener("click", handleContinueClick, {
     capture: false,
     once: true,
   });
 
-  speaker.addEventListener('click', handleSpeakerClick, {
+  speaker.addEventListener("click", handleSpeakerClick, {
     capture: false,
     once: false,
   });
@@ -407,14 +423,14 @@ document.addEventListener('DOMContentLoaded', function () {
             ideal: 720,
             max: 1080,
           },
-          facingMode: 'user',
+          facingMode: "user",
         },
       });
     }
 
     await pause(2500);
 
-    button.style.display = 'inline';
+    button.style.display = "inline";
     button.disabled = false;
   };
   startTrials();
